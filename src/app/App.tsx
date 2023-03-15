1fc3d0663d98e4bfcc8333688486a868fa639b6f
DrobyshSV <austin@tut.by>
2023-03-15 12:28:37 +0300

    feat: add AppLink UI Kit and styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import {useTheme} from 'app/providers/ThemeProbider';
 import {classNames} from 'shared/lib/classNames/classNames';
 import {AppRouter} from 'app/providers/router';
 
 import './styles/index.scss';
+import {Navbar} from 'widgets/Navbar';
 
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Main Page</Link>
-      <Link to={'/about'}>About Page</Link>
+      <Navbar/>
       <AppRouter/>
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
