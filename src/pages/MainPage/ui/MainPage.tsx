7aa49e46bf08fc3feef5a56e7d087651026a2e27
DrobyshSV <austin@tut.by>
2023-03-28 17:44:44 +0300

    feat: add ErrorBoundary, PageError, BugButton

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { BugButton } from 'app/providers/ErrorBoundary';
 
-const MainPage = () => (
-  <div>
-    MainPage
-  </div>
-);
+const MainPage = () => {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <BugButton />
+      {t('MainPage')}
+    </div>
+  );
+};
 
 export default MainPage;
