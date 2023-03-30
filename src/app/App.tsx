59b11c9f7be376ec2289b9385c183edee88b541e
DrobyshSV <austin@tut.by>
2023-03-30 17:57:18 +0300

    feat: add Modal component with Portal, fix ThemeProvider  and ThemeDecorator, update screens

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,6 @@ import React, { Suspense } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
-
-import './styles/index.scss';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,12 +11,25 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
   return (
     <div className={classNames(styles.Navbar, {}, [className])}>
-      <div className={styles.links}>
-        <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={styles.mainLink}>{t('Main Page')}</AppLink>
-        <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('About Page')}</AppLink>
-      </div>
+      <Button
+        theme={ThemeButton.CLEAR_INVERTED}
+        className={styles.links}
+        onClick={onToggleModal}
+      >
+        {t('Sigh in')}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        {/* eslint-disable-next-line */}
+        {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.')}
+      </Modal>
     </div>
   );
 };
