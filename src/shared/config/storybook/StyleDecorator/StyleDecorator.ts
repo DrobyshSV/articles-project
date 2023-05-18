a0f6bbe824530f0f58395cefb815c8591d63838d
DrobyshSV <austin@tut.by>
2023-05-18 13:55:39 +0300

    fix: update screens

--- src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
+++ src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
@@ -1,4 +1,4 @@
-import '../styles/index.scss';
 import { Story } from '@storybook/react';
+import '../../../../app/styles/index.scss';
 
 export const StyleDecorator = (story: () => Story) => story();
